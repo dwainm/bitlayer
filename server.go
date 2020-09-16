46f4f352726afc48ebbb90d75505b10fb55864e5
Dwain Maralack <dwain.maralack@gmail.com>
2020-09-16 17:16:38 +0200

    Add the basic structs for layer and the idea of how a picture will be made up out of

---
+++ server.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+
+type layer struct {
+	color []byte // #ffffff
+	fill [] //	fill : array of array of coordinates [[0,15] ] max 256 coordinates
+}
+
+func main() {
+	const picture = []layer
+
+	// create layers
+}
